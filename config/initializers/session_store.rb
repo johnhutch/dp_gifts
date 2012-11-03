9a03583024e8f9b2e64699dc46e1a0b2431e544a
John Hutchinson <johnhutch@gmail.com>
2012-11-02 23:36:23 -0400

    missed two tinderses. replaced with dp_gifts.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-DPGifts::Application.config.session_store :cookie_store, :key => '_tinder_session'
+DPGifts::Application.config.session_store :cookie_store, :key => '_dp_gifts_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
