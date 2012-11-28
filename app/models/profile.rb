18f0cfc027e43fe8570a04ac83773c20680707ee
John Hutchinson <johnhutch@gmail.com>
2012-11-28 00:31:34 -0500

    fixed profile mass assignment and profile edit form

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,5 @@
 class Profile < ActiveRecord::Base
-  attr_accessible :address, :amazon_wishlist, :bio, :twitter, :user_id, :website
+  attr_accessible :address, :amazon_wishlist, :bio, :twitter, :user_id, :website, :likes
 
   belongs_to :user
 end
