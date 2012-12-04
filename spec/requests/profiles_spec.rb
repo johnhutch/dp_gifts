71b012a1d9bb13868bb4c4af1a801a2a3e421195
John Hutchinson <johnhutch@gmail.com>
2012-12-04 13:20:01 -0500

    updated profile form to fix profile submit button and colorize buttons.

--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -9,11 +9,11 @@ describe "Profiles" do
       visit edit_user_registration_path
 
       fill_in "Tell us a bit about yourself.", :with => "Hi there. Lemme tell you about myself."
-      fill_in "Things I like and things I'm allergic to:", :with => "I like all kinds of things."
+      fill_in "What are some things you like?", :with => "I like all kinds of things."
       fill_in "Mailing Address", :with => "21 jump st.\nGenorocity, Denial 12345\nDonation"
-      fill_in "Amazon wishlist", :with => "http://www.amazon.com/blah"
-      fill_in "Twitter Account", :with => "johnhutch"
-      fill_in "Your Website", :with => "http://www.johnhutch.com"
+      fill_in "Do you have a wishlist on amazon.com?", :with => "http://www.amazon.com/blah"
+      fill_in "Do you have a twitter account?", :with => "johnhutch"
+      fill_in "Do you have a website?", :with => "http://www.johnhutch.com"
       click_button I18n.t('buttons.update_profile')
       page.should have_content(I18n.t('flash.profile_updated'))
       page.should have_content('21 jump st.')
